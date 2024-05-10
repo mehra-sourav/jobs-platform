c0c3e036c59f07fe20f9b41d8b6b93515aa6b778
Sourav <1997mehra@gmail.com>
2024-05-10 16:19:05 +0530

    feat: created basic structure of the app

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -1,11 +1,17 @@
+import { Grid } from "@mui/material";
+
 import Card from "@/components/Card";
 
 const CardList = () => {
   return (
-    <div>
-      <Card />
-      <Card />
-    </div>
+    <Grid container spacing={{ xs: 2, md: 3 }}>
+      <Grid item xs={12} sm={4} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={4} md={4}>
+        <Card />
+      </Grid>
+    </Grid>
   );
 };
 
--- src/components/FilterList/index.jsx
+++ src/components/FilterList/index.jsx
@@ -1,11 +1,15 @@
 import Filter from "@/components/Filter";
+import { Stack } from "@mui/material";
 
 const FilterList = () => {
   return (
-    <div>
-      <Filter />
-      <Filter />
-    </div>
+    <Stack
+      direction={{ xs: "column", sm: "row" }}
+      spacing={{ xs: 1, sm: 2 }}
+      sx={{ border: "1px solid red", mb: 5 }}
+    >
+      Filter List
+    </Stack>
   );
 };
 
