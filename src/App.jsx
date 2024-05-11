fbdd647d246a2aa9248d5baac443308072e557cb
Sourav <1997mehra@gmail.com>
2024-05-11 17:38:51 +0530

    feat: created basic card grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import "./App.css";
+import { useEffect, useState } from "react";
 import { Container } from "@mui/material";
 import CardList from "@/components/CardList";
 import FilterList from "@/components/FilterList";
+import { getJobs } from "@/api";
+import "./App.css";
+
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    getJobs().then((result) => {
+      setJobs(result.jdList);
+    });
+  }, []);
+
   return (
     <Container maxWidth="xl">
       <FilterList />
-      <CardList />
+      <CardList jobs={jobs} />
     </Container>
   );
 }
--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -2,15 +2,14 @@ import { Grid } from "@mui/material";
 
 import Card from "@/components/Card";
 
-const CardList = () => {
+const CardList = ({ jobs }) => {
   return (
     <Grid container spacing={{ xs: 2, md: 3 }}>
-      <Grid item xs={12} sm={4} md={4}>
-        <Card />
-      </Grid>
-      <Grid item xs={12} sm={4} md={4}>
-        <Card />
-      </Grid>
+      {jobs.map((job, idx) => (
+        <Grid key={idx} item xs={12} sm={4} md={4}>
+          <Card key={idx} job={job} />
+        </Grid>
+      ))}
     </Grid>
   );
 };
